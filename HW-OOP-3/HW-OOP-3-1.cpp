67367619c8b89d2747aed545f4f28fa685632ca8
Dyanavio <dimant2018@gmail.com>
2025-04-19 12:25:51 +0300

    HW-OOP-3

---
+++ HW-OOP-3/HW-OOP-3-1.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <list>
+#include <string.h>
+#include "color.hpp"
+
+using namespace std;
+
+struct Student
+{
+	string surname;
+	int year;
+	double calculus; // математический анализ
+	double algebra;
+
+	Student(string surname, int year, double calculus, double algebra)
+	{
+		this->surname = surname;
+		this->year = year;
+		this->calculus = calculus;
+		this->algebra = algebra;
+	}
+};
+
+void printList(list<Student>& students)
+{
+	std::list<Student>::iterator it;
+	for (it = students.begin(); it != students.end(); it++)
+	{
+		cout << (*it).surname << " " << (*it).year << " | Calculus: " << (*it).calculus << " | Algebra: " << (*it).algebra << endl;
+	}
+	cout << endl;
+}
+
+bool surnamePredicate(const Student& first, const Student& second)
+{
+	/*int i = 0;
+	while ((i < first.surname.length()) && (i < second.surname.length()))
+	{
+		if (tolower(first.surname[i]) < tolower(second.surname[i])) return true;
+		else if (tolower(first.surname[i]) > tolower(second.surname[i])) return false;
+		++i;
+	}
+	return (first.surname.length() < second.surname.length());*/
+	if (first.surname <= second.surname) return true;
+	else return false;
+}
+
+bool yearPredicate(const Student& first, const Student& second)
+{
+	if (first.year < second.year) return true;
+	else return false;
+}
+
+void transfer(list<Student>& students)
+{
+	std::list<Student>::iterator it;
+	for (it = students.begin(); it != students.end(); it++)
+	{
+		if ((*it).calculus >= 80 && (*it).algebra >= 80) (*it).year++; // Поменял условие на 80
+	}
+}
+
+bool deletePredicate(const Student& student)
+{
+	if (student.algebra <= 80 || student.calculus <= 80) return true;
+	return false;
+}
+
+int main()
+{
+	list<Student> students;
+
+	students.push_back(Student("Cranel", 1, 85.5, 88.0));
+	students.push_back(Student("Lion", 1, 90.0, 88.5));
+	students.push_back(Student("Asahina", 1, 76.0, 82.3));
+	students.push_back(Student("Aqua", 1, 92.1, 85.4));
+	students.push_back(Student("Akane", 1, 80.5, 80.0));
+	students.push_back(Student("Arima", 2, 88.0, 91.2));
+	students.push_back(Student("Polnaref", 2, 65.5, 89.9));
+	students.push_back(Student("Alhaitham", 2, 84.3, 87.7));
+	students.push_back(Student("Sivukhin", 2, 91.8, 90.0));
+	students.push_back(Student("Landau", 2, 86.6, 83.5));
+
+	printList(students);
+
+	cout << dye::light_blue("----- Sorted List Alphabetically -----") << endl;
+	students.sort(surnamePredicate);
+	printList(students);
+
+	cout << dye::light_red("----- Sorted List By Year -----") << endl;
+	students.sort(yearPredicate);
+	printList(students);
+
+	transfer(students);
+	cout << dye::light_yellow("----- Transferred -----") << endl;
+	printList(students);
+
+
+	students.remove_if(deletePredicate);
+	cout << dye::red("----- List After Deleting Students -----") << endl;
+	printList(students);
+
+}
---
+++ HW-OOP-3/HW-OOP-3.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <list>
+#include "color.hpp"
+
+using namespace std;
+
+bool lastDigitPredicate(int number) { return number % 10 > 4; }
+
+static void printList(list<int>& list)
+{
+	std::list<int>::iterator it;
+	for (it = list.begin(); it != list.end(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+}
+
+int main()
+{
+	srand(time(NULL));
+
+	list<int> list;
+	int input;
+	cout << dye::yellow("----- Filling in the list -----") << endl;
+	do
+	{
+		cout << "Enter value: ";
+		cin >> input;
+		list.push_back(input);
+	} while (input);
+
+	cout << dye::green("----- List Output -----") << endl;
+	printList(list);
+
+	for (int i = 0; i < 3; i++)
+	{
+		list.push_front(rand() % 25);
+	}
+
+	cout << dye::red("\n----- List Output After PUSH-FRONT -----") << endl;
+	printList(list);
+	
+	list.sort();
+	cout << dye::blue("\n----- Sorted List -----") << endl;
+	printList(list);
+
+	list.pop_front();
+	cout << dye::light_aqua("\n----- List POP-FRONT -----") << endl;
+	printList(list);
+
+	cout << endl << dye::light_blue("Front list element: ") << list.front() << endl;
+
+	list.pop_back();
+	cout << dye::light_green("\n----- List POP-BACK -----") << endl;
+	printList(list);
+
+	cout << endl << dye::purple("Back list element: ") << list.back() << endl;
+	cout << endl << dye::light_red("List size: ") << list.size() << endl;
+
+	list.remove(0);
+	cout << dye::aqua("\n----- Deleting all ZEROS -----") << endl;
+	printList(list);
+
+	list.remove_if(lastDigitPredicate);
+	cout << dye::light_purple("\n----- Deleting all number with last digit greater than 4 -----") << endl;
+	printList(list);
+
+}
